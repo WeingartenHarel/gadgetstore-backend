ed951e0226b0b64ab76b7690166bd64dcaba46fb
harelwn <harelwn@users.noreply.github.com>
2021-01-05 14:49:07 +0200

    v01

---
+++ api/product/product.routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {requireAuth, requireAdmin,requireUser} = require('../../middlewares/requireAuth.middleware')
+const {addProduct,updateProduct, getProducts, getProduct, deleteProduct} = require('./product.controller')
+const router = express.Router()
+
+// middleware that is specific to this router
+
+//router.use(requireUser)
+//router.use(requireAdmin)
+router.get('/', getProducts) //requireUser
+router.get('/:id', getProduct) //requireUser
+
+router.post('/' , addProduct) //,requireAdmin
+router.put('/:id', updateProduct) //, requireAdmin
+router.delete('/:id', deleteProduct) //, requireAdmin
+
+module.exports = router
---
+++ api/product/product.service.js
@@ -0,0 +1,100 @@
+const dbService = require('../../services/db.service')
+const ObjectId = require('mongodb').ObjectId
+ 
+ 
+async function query(filterBy = {}) {
+    // TODO: Build the criteria with $regex
+     const criteria = _buildCriteria(filterBy)
+    const collection = await dbService.getCollection('product')
+
+    try {
+         const products = await collection.find().toArray();
+       // var products = await collection.aggregate([
+       //     {
+       //         $match: filterBy
+       //     }
+       // ]).toArray()
+        ////console.log('products',products)
+        return products
+    } catch (err) {
+        //console.log('ERROR: cannot find products')
+        throw err;
+    }
+}
+
+
+async function getById(productId) {
+    const collection = await dbService.getCollection('product')
+    try {
+        const product = await collection.findOne({ '_id': ObjectId(productId) })
+        //delete product.password
+
+       // product.givenReviews = await reviewService.query({ byUserId: ObjectId(product._id) })
+       // product.givenReviews = product.givenReviews.map(review => {
+       //     delete review.byUser
+       //     return review
+       // })
+
+    //    //console.log('finding product' ,product)
+        return product
+    } catch (err) {
+        //console.log(`ERROR: while finding product ${productId}`)
+        throw err;
+    }
+}
+ 
+async function remove(productId) {
+    const collection = await dbService.getCollection('product')
+    try {
+        //bjectId(productId)
+        await collection.deleteOne({ "_id": ObjectId(productId) })
+    } catch (err) {
+        //console.log(`ERROR: cannot remove product ${productId}`)
+        throw err;
+    }
+}
+
+
+async function add(product) {
+    const collection = await dbService.getCollection('product')
+    try {
+        await collection.insertOne(product);
+        return product;
+    } catch (err) {
+        //console.log(`ERROR: cannot insert product`)
+        throw err;
+    }
+}
+
+async function update(product) {
+    //console.log('MIX BACK SERVICE : ',product);
+    const collection = await dbService.getCollection('product')
+    product._id = ObjectId(product._id);
+    try {
+        await collection.updateOne({ _id: product._id }, { $set: product })
+        return product
+    } catch (err) {
+        //console.log(`ERROR: cannot update product ${product._id}`)
+        throw err;
+    }
+}
+
+
+function _buildCriteria(filterBy) {
+    const criteria = {};
+    if (filterBy.name) {
+        criteria.name = filterBy.name
+    }
+    
+    return criteria;
+}
+
+module.exports = {
+    query,
+    getById,
+    remove,
+    add,
+    update
+}
+
+
